20c2885ec4d930a31302868c8aaf3f76fb085ec6
Rodrigo <rodrigo.blizzard@hotmail.com>
2020-07-08 11:57:59 -0300

    Projeto criado

---
+++ src/main/java/com/visionnareTest1/visionnareTest1/VisionnareTest1Application.java
@@ -0,0 +1,13 @@
+package com.visionnareTest1.visionnareTest1;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class VisionnareTest1Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(VisionnareTest1Application.class, args);
+	}
+
+}
---
+++ src/test/java/com/visionnareTest1/visionnareTest1/VisionnareTest1ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.visionnareTest1.visionnareTest1;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class VisionnareTest1ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
